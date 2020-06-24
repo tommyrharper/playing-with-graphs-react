37bb03f90a39bc5ebf2ff13261dd7bb90fee80eb
tommyrharper <tommyrharper@gmail.com>
2020-06-24 19:20:33 +0100

    Accept multiple inputs in Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,18 +3,27 @@ import React from 'react'
 class ReactForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''}
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    }
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value})
+  handleInputChange(event) {
+    const target = event.target
+    const value = target.name === 'isGoing' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   handleSubmit(event) {
-    alert('A name was submitted:' + this.state.value)
+    alert('Form was submitted, guests:' + this.state.numberOfGuests)
     event.preventDefault();
   }
 
@@ -22,8 +31,21 @@ class ReactForm extends React.Component {
     return(
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          Is going:
+          <input 
+          name="isGoing"
+          type="checkbox" 
+          checked={this.state.isGoing}
+          onChange={this.handleInputChange} />
+        </label>
+        <br/>
+        <label>
+          Number of guests:
+          <input
+          name="numberOfGuests"
+          type="number"
+          value={this.state.numberOfGuests}
+          onChange={this.handleInputChange} />
         </label>
         <input type="submit" value="Submit"/>
       </form>
