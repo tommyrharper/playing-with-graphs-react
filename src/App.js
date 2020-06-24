2a12b4e1be88b47d9699b2c943db7057a16ef91c
tommyrharper <tommyrharper@gmail.com>
2020-06-24 17:59:55 +0100

    Remove getChartData function

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
   }
 
   componentWillMount(){
-    // this.getChartData()
     this.generateChartData(10_000, 1.1, 10)
   }
 
@@ -30,36 +29,6 @@ class App extends Component {
           {
             label:'Money in £',
             data:arrayMoney,
-            backgroundColor:[
-              'rgba(54, 162, 235, 0.6)'
-            ]
-          }
-        ]
-      }
-    })
-  }
-
-  getChartData(){
-    // Ajax call here if getting information from API for example
-    this.setState({
-      chartData:{
-        labels: [2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030],
-        datasets:[
-          {
-            label:'Money in £',
-            data:[
-              10000,
-              11000,
-              12100,
-              13310,
-              14641,
-              16105.1,
-              17715.61,
-              19487.171,
-              21435.8881,
-              23579.47691,
-              25937.4246
-            ],
             backgroundColor:[
               'rgba(54, 162, 235, 0.6)'
             ]
