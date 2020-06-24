c2ddbd50ad92907d7fb02628c02ae1b9bbeb05fa
tommyrharper <tommyrharper@gmail.com>
2020-06-24 16:34:45 +0100

    Import Chart component into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chart from './components/Chart.js'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Chart/>
       </header>
     </div>
   );
