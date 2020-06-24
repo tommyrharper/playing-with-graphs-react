c87add6abd437daebcf73ab2f78d953312fdbbfe
tommyrharper <tommyrharper@gmail.com>
2020-06-24 16:33:25 +0100

    Create Chart component

---
+++ src/components/Chart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Doughnut, Bar, Line, Pie} from 'react-chartjs-2';
+
+class Chart extends Component {
+  render(){
+    return(
+      <div className="cart">
+        CHART COMPONENT
+      </div>
+    )
+  }
+}
