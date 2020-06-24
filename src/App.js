b8d3939ea0f2676c2200ce6eef53a459d9691d18
tommyrharper <tommyrharper@gmail.com>
2020-06-24 16:58:12 +0100

    Fix data in App.js

--- src/App.js
+++ src/App.js
@@ -10,32 +10,38 @@ class App extends Component {
     };
   }
 
+  componentWillMount(){
+    this.getChartData()
+  }
+
   getChartData(){
     // Ajax call here if getting information from API for example
     this.setState({
-      labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
-      datasets:[
-        {
-          label:'Population',
-          data:[
-            617594,
-            181045,
-            153060,
-            106519,
-            105162,
-            95072
-          ],
-          backgroundColor:[
-            'rgba(255, 99, 132, 0.6)',
-            'rgba(54, 162, 235, 0.6)',
-            'rgba(255, 206, 86, 0.6)',
-            'rgba(75, 192, 192, 0.6)',
-            'rgba(153, 102, 255, 0.6)',
-            'rgba(255, 159, 64, 0.6)',
-            'rgba(255, 99, 132, 0.6)'
-          ]
-        }
-      ]
+      chartData:{
+        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets:[
+          {
+            label:'Population',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
     })
   }
 
@@ -44,6 +50,7 @@ class App extends Component {
       <div className="App">
         <Chart
         legendPosition='bottom'
+        chartData={this.state.chartData}
         />
       </div>
     );
