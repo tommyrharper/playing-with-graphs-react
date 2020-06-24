e92c1c9f31a3cbd9c9c363e81de5dfc48ad8130a
tommyrharper <tommyrharper@gmail.com>
2020-06-24 20:08:41 +0100

    Remove props for Chart Ledgend position from App

--- src/App.js
+++ src/App.js
@@ -66,7 +66,6 @@ class App extends Component {
       />
       <div className="App">
         <Chart
-          legendPosition='bottom'
           chartData={this.state.chartData}
         />
       </div>
