08b95b97b18e2a9894ea6c3a13d015f338d59685
tommyrharper <tommyrharper@gmail.com>
2020-06-24 21:19:38 +0100

    Create InitialChartData.js

---
+++ src/components/InitialChartData.js
@@ -0,0 +1,14 @@
+const initialChartData = {
+  labels: [2020, 2021, 2022, 2023, 2024, 2025],
+  datasets:[
+    {
+      label:'Money in £',
+      data:[10000, 11000, 12100, 13310, 14641, 16105],
+      backgroundColor:[
+        'rgba(54, 162, 235, 0.4)'
+      ]
+    }
+  ]
+}
+
+export default initialChartData
