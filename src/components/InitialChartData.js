ba8e834ee7c1a71c2956d9c3dfbf2c0e45e18a51
tommyrharper <tommyrharper@gmail.com>
2020-07-28 17:40:14 +0100

    Remove initial chart data from datasets

--- src/components/InitialChartData.js
+++ src/components/InitialChartData.js
@@ -1,20 +1,7 @@
 const initialChartData = {
   labels: [2020, 2021, 2022, 2023, 2024, 2025],
   datasets:[
-    // {
-    //   label:'Money in £',
-    //   data:[10000, 11000, 12100, 13310, 14641, 16105],
-    //   backgroundColor:[
-    //     'rgba(54, 162, 235, 0.4)'
-    //   ]
-    // },
-    // {
-    //   label:'Money in £',
-    //   data:[5000, 6000, 7000, 8000, 12000, 18000],
-    //   backgroundColor:[
-    //     'rgba(54, 162, 235, 0.4)'
-    //   ]
-    // }
+
   ]
 }
 
