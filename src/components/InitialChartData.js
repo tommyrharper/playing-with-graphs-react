cad5728d2034c374454b6d97cb3cf28f28bf531f
tommyrharper <tommyrharper@gmail.com>
2020-07-28 21:03:13 +0100

    Save empty initialChartData with comments

--- src/components/InitialChartData.js
+++ src/components/InitialChartData.js
@@ -1,8 +1,23 @@
 const initialChartData = {
   labels: [],
   datasets:[
-
+    // {
+    //   // label:'Money in £',
+    //   label:false,
+    //   fill: false,
+    //   // borderColor: 'none',
+    //   data: [0],
+    // }
   ]
 }
 
 export default initialChartData
+
+
+// let savedDataObject = {
+//   label:'Money in £',
+//   data: yAxis,
+//   backgroundColor:[
+//     'rgba(54, 162, 235, 0.4)'
+//   ]
+// }
